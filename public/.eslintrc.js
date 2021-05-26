b8ff0b8587e95c067b374b1bd285509a5475c127
Felipe Defacio <Felipe.Defacio@la.logicalis.com>
2021-05-25 22:31:30 -0300

    Initial commit

---
+++ public/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "parserOptions": {
+      "sourceType": "script"
+    },
+    "env": {
+      "browser": true,
+      "jquery": true,
+      "node": false
+    },
+    "rules": {
+      "no-var": "off",
+      "prefer-const": "off"
+    }
+  }
+  
